d6cc2fd27a4c384105a746ca353ff66e4011abca
Maxim Lyahovskij <asmaximax256@gmail.com>
2022-03-07 21:01:51 +0300

    добавил файлы основной либы

---
+++ src/be2r_cmpc_unitree/be2r_cmpc_unitree.cpp
@@ -0,0 +1,334 @@
+#include "be2r_cmpc_unitree.hpp"
+
+using namespace std;
+
+Body_Manager::Body_Manager(RobotController* robot_ctrl)
+{
+  _robot_ctrl = robot_ctrl;
+  _userControlParameters = robot_ctrl->getUserControlParameters();
+}
+
+Body_Manager::~Body_Manager()
+{
+  delete _legController;
+  delete _stateEstimator;
+}
+
+void Body_Manager::init()
+{
+  _initSubscribers();
+  _initPublishers();
+
+  printf("[Hardware Bridge] Loading parameters from file...\n");
+
+  try
+  {
+    controlParameters.initializeFromYamlFile(THIS_COM "config/mini-cheetah-defaults.yaml");
+  }
+  catch (std::exception& e)
+  {
+    printf("Failed to initialize robot parameters from yaml file: %s\n", e.what());
+    exit(1);
+  }
+
+  if (!controlParameters.isFullyInitialized())
+  {
+    printf("Failed to initialize all robot parameters\n");
+    exit(1);
+  }
+
+  printf("Loaded robot parameters\n");
+
+  if (_userControlParameters)
+  {
+    try
+    {
+      _userControlParameters->initializeFromYamlFile(THIS_COM "config/mc-mit-ctrl-user-parameters.yaml");
+    }
+    catch (std::exception& e)
+    {
+      printf("Failed to initialize user parameters from yaml file: %s\n", e.what());
+      exit(1);
+    }
+
+    if (!_userControlParameters->isFullyInitialized())
+    {
+      printf("Failed to initialize all user parameters\n");
+      exit(1);
+    }
+
+    printf("Loaded user parameters\n");
+  }
+  else
+  {
+    printf("Did not load user parameters because there aren't any\n");
+  }
+
+  _quadruped = buildMiniCheetah<float>();
+
+  // Initialize the model and robot data
+  _model = _quadruped.buildModel();
+  // _jpos_initializer = new JPosInitializer<float>(3., controlParameters->controller_dt); // HERE mb delete this?
+
+  // Always initialize the leg controller and state entimator
+  _legController = new LegController<float>(_quadruped);
+  _stateEstimator = new StateEstimatorContainer<float>(&vectorNavData, _legController->datas, &_stateEstimate, &controlParameters);
+  initializeStateEstimator();
+
+  // Initialize the DesiredStateCommand object
+  _desiredStateCommand = new DesiredStateCommand<float>(&driverCommand, &rc_control, &controlParameters, &_stateEstimate, controlParameters.controller_dt);
+
+  // Controller initializations
+  _robot_ctrl->_model = &_model;
+  _robot_ctrl->_quadruped = &_quadruped;
+  _robot_ctrl->_legController = _legController;
+  _robot_ctrl->_stateEstimator = _stateEstimator;
+  _robot_ctrl->_stateEstimate = &_stateEstimate;
+  // _robot_ctrl->_visualizationData = visualizationData;
+  _robot_ctrl->_robotType = RobotType::MINI_CHEETAH;
+  _robot_ctrl->_driverCommand = &driverCommand;
+  _robot_ctrl->_controlParameters = &controlParameters;
+  _robot_ctrl->_desiredStateCommand = _desiredStateCommand;
+
+  _robot_ctrl->initializeController();
+}
+
+void Body_Manager::run()
+{
+  // ROS_INFO("Run");
+  // cout << "[Body Manager] Run start" << endl;
+
+  // Run the state estimator step
+  _stateEstimator->run();
+
+  // Update the data from the robot
+  setupStep();
+
+  // cout << "[Body Manager] Setup step done" << endl;
+
+  static int count_ini(0);
+  ++count_ini;
+
+  if (count_ini < 10)
+  {
+    _legController->setEnabled(false);
+  }
+  else if (20 < count_ini && count_ini < 30)
+  {
+    _legController->setEnabled(false);
+  }
+  else if (40 < count_ini && count_ini < 50)
+  {
+    _legController->setEnabled(false);
+  }
+  else
+  {
+    _legController->setEnabled(true);
+
+    // // Run Control
+    // _robot_ctrl->runController();
+
+    // if (!is_stand)
+    if (0)
+    {
+      Mat3<float> kpMat;
+      Mat3<float> kdMat;
+      // Update the jpos feedback gains
+
+      kpMat << 5, 0, 0, 0, 5, 0, 0, 0, 5;
+      kdMat << 0.1, 0, 0, 0, 0.1, 0, 0, 0, 0.1;
+
+      for (int leg = 0; leg < 4; leg++)
+      {
+        _legController->commands[leg].kpJoint = kpMat;
+        _legController->commands[leg].kdJoint = kdMat;
+      }
+    }
+    else
+    {
+      // Run Control
+      _robot_ctrl->runController();
+
+      // cout << "[Body Manager] CTRL loop done" << endl;
+    }
+  }
+
+  // Sets the leg controller commands for the robot appropriate commands
+  finalizeStep();
+
+  if (count_ini > 100 && count_ini < 2000)
+  {
+    ROS_INFO_STREAM_ONCE("Stand up " << count_ini);
+
+    controlParameters.control_mode = 1;
+  }
+  else if (count_ini > 2000)
+  {
+    ROS_INFO_STREAM_ONCE("Locomotion " << count_ini);
+
+    controlParameters.control_mode = 3;
+    // controlParameters.control_mode = 4;
+  }
+}
+
+void Body_Manager::setupStep()
+{
+  _legController->updateData(&spiData);
+
+  // Setup the leg controller for a new iteration
+  _legController->zeroCommand();
+  _legController->setEnabled(true);
+  _legController->setMaxTorqueCheetah3(208.5);
+
+  // get_rc_control_settings(&rc_control);
+
+  // todo safety checks, sanity checks, etc...
+}
+
+void Body_Manager::finalizeStep()
+{
+
+  _legController->updateCommand(&spiCommand);
+
+  _iterations++;
+
+  for (int i = 0; i < 4; i++)
+  {
+    _torqueCalculator(&spiCommand, &spiData, &_spi_torque, i);
+  }
+
+  static unitree_legged_msgs::LowCmd _low_cmd;
+
+  for (uint8_t leg_num = 0; leg_num < 4; leg_num++)
+  {
+    _low_cmd.motorCmd[leg_num * 3 + 0].tau = _spi_torque.tau_abad[leg_num];
+    _low_cmd.motorCmd[leg_num * 3 + 1].tau = -_spi_torque.tau_hip[leg_num];
+    _low_cmd.motorCmd[leg_num * 3 + 2].tau = -_spi_torque.tau_knee[leg_num];
+  }
+
+  _pub_low_cmd.publish(_low_cmd);
+}
+
+/*!
+ * Reset the state estimator in the given mode.
+ * @param cheaterMode
+ */
+void Body_Manager::initializeStateEstimator()
+{
+  _stateEstimator->removeAllEstimators();
+  _stateEstimator->addEstimator<ContactEstimator<float>>();
+  Vec4<float> contactDefault;
+  contactDefault << 0.5, 0.5, 0.5, 0.5;
+  _stateEstimator->setContactPhase(contactDefault);
+
+  _stateEstimator->addEstimator<VectorNavOrientationEstimator<float>>();
+  _stateEstimator->addEstimator<LinearKFPositionVelocityEstimator<float>>();
+}
+
+void Body_Manager::_initSubscribers()
+{
+  _sub_low_state = _nh.subscribe("/low_state", 1, &Body_Manager::_lowStateCallback, this, ros::TransportHints().tcpNoDelay(true));
+}
+
+void Body_Manager::_initPublishers()
+{
+  _pub_low_cmd = _nh.advertise<unitree_legged_msgs::LowCmd>("/low_cmd", 1);
+}
+
+void Body_Manager::_lowStateCallback(unitree_legged_msgs::LowState msg)
+{
+  // ROS_INFO("lsc");
+
+  for (uint8_t leg_num = 0; leg_num < 4; leg_num++)
+  {
+    spiData.q_abad[leg_num] = msg.motorState[leg_num * 3 + 0].q;
+    spiData.q_hip[leg_num] = -msg.motorState[leg_num * 3 + 1].q;
+    spiData.q_knee[leg_num] = -msg.motorState[leg_num * 3 + 2].q;
+
+    spiData.qd_abad[leg_num] = msg.motorState[leg_num * 3 + 0].dq;
+    spiData.qd_hip[leg_num] = -msg.motorState[leg_num * 3 + 1].dq;
+    spiData.qd_knee[leg_num] = -msg.motorState[leg_num * 3 + 2].dq;
+  }
+
+  is_stand = msg.levelFlag;
+
+  vectorNavData.gyro[0] = msg.imu.gyroscope[0];
+  vectorNavData.gyro[1] = msg.imu.gyroscope[1];
+  vectorNavData.gyro[2] = msg.imu.gyroscope[2];
+
+  vectorNavData.accelerometer[0] = msg.imu.accelerometer[0];
+  vectorNavData.accelerometer[1] = msg.imu.accelerometer[1];
+  vectorNavData.accelerometer[2] = msg.imu.accelerometer[2];
+
+  // vectorNavData.quat[0] = msg.imu.quaternion[0]; //w
+  // vectorNavData.quat[1] = msg.imu.quaternion[1]; //x
+  // vectorNavData.quat[2] = msg.imu.quaternion[2]; //y
+  // vectorNavData.quat[3] = msg.imu.quaternion[3]; //z
+
+  vectorNavData.quat[0] = msg.imu.quaternion[1]; //w
+  vectorNavData.quat[1] = msg.imu.quaternion[2]; //x
+  vectorNavData.quat[2] = msg.imu.quaternion[3]; //y
+  vectorNavData.quat[3] = msg.imu.quaternion[0]; //z
+}
+
+/*!
+ * Emulate the spi board to estimate the torque.
+ */
+void Body_Manager::_torqueCalculator(SpiCommand* cmd, SpiData* data, spi_torque_t* torque_out, int board_num)
+{
+  torque_out->tau_abad[board_num] = cmd->kp_abad[board_num] * (cmd->q_des_abad[board_num] - data->q_abad[board_num]) +
+                                    cmd->kd_abad[board_num] * (cmd->qd_des_abad[board_num] - data->qd_abad[board_num]) +
+                                    cmd->tau_abad_ff[board_num];
+
+  torque_out->tau_hip[board_num] = cmd->kp_hip[board_num] * (cmd->q_des_hip[board_num] - data->q_hip[board_num]) +
+                                   cmd->kd_hip[board_num] * (cmd->qd_des_hip[board_num] - data->qd_hip[board_num]) +
+                                   cmd->tau_hip_ff[board_num];
+
+  torque_out->tau_knee[board_num] = cmd->kp_knee[board_num] * (cmd->q_des_knee[board_num] - data->q_knee[board_num]) +
+                                    cmd->kd_knee[board_num] * (cmd->qd_des_knee[board_num] - data->qd_knee[board_num]) +
+                                    cmd->tau_knee_ff[board_num];
+
+  // const float* torque_limits = disabled_torque;
+  const float* torque_limits = max_torque;
+  // const float* torque_limits = max_max_torque;
+
+  // if (cmd->flags[board_num] & 0b1)
+  // {
+  //   if (cmd->flags[board_num] & 0b10)
+  //     torque_limits = wimp_torque;
+  //   else
+  //     torque_limits = max_torque;
+  // }
+
+  if (torque_out->tau_abad[board_num] > torque_limits[0])
+  {
+    torque_out->tau_abad[board_num] = torque_limits[0];
+  }
+  if (torque_out->tau_abad[board_num] < -torque_limits[0])
+  {
+    torque_out->tau_abad[board_num] = -torque_limits[0];
+  }
+  if (torque_out->tau_hip[board_num] > torque_limits[1])
+  {
+    torque_out->tau_hip[board_num] = torque_limits[1];
+  }
+  if (torque_out->tau_hip[board_num] < -torque_limits[1])
+  {
+    torque_out->tau_hip[board_num] = -torque_limits[1];
+  }
+  if (torque_out->tau_knee[board_num] > torque_limits[2])
+  {
+    torque_out->tau_knee[board_num] = torque_limits[2];
+  }
+  if (torque_out->tau_knee[board_num] < -torque_limits[2])
+  {
+    torque_out->tau_knee[board_num] = -torque_limits[2];
+  }
+
+  // float q0_e = cmd->q_des_abad[board_num] - data->q_abad[board_num];
+  // float q1_e = cmd->q_des_hip[board_num] - data->q_hip[board_num];
+  // float q2_e = cmd->q_des_knee[board_num] - data->q_knee[board_num];
+
+  // cout << "Leg: " << board_num << " q0_e: " << q0_e << " q1_e: " << q1_e << " q2_e: " << q2_e << endl;
+  // cout << "Leg: " << board_num << " t0: " << torque_out->tau_abad[board_num] << " t1: " << torque_out->tau_hip[board_num] << " t2: " << torque_out->tau_knee[board_num] << endl;
+}
---
+++ src/be2r_cmpc_unitree/be2r_cmpc_unitree.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+//C++
+#include <iostream>
+
+//ROS
+#include <ros/ros.h>
+#include <unitree_legged_msgs/LowCmd.h>
+#include <unitree_legged_msgs/LowState.h>
+
+//MIT
+#include "Controllers/StateEstimatorContainer.h"
+#include "LegController.h"
+#include "MiniCheetah.h"
+#include "RobotParameters.h"
+// #include "RobotController.h"
+#include "Configuration.h"
+#include "MIT_Controller.hpp"
+#include "spi_command_t.hpp"
+#include "spi_data_t.hpp"
+#include "spi_torque_t.hpp"
+
+#include <ros/transport_hints.h>
+
+#include "Controllers/ContactEstimator.h"
+#include "Controllers/DesiredStateCommand.h"
+#include "Controllers/OrientationEstimator.h"
+#include "Controllers/PositionVelocityEstimator.h"
+
+#define MAIN_LOOP_RATE 500
+
+const float max_torque[3] = {17.f, 17.f, 26.f};        // TODO CHECK WITH BEN
+const float max_max_torque[3] = {170.f, 170.f, 260.f}; // TODO CHECK WITH BEN
+const float wimp_torque[3] = {6.f, 6.f, 6.f};          // TODO CHECK WITH BEN
+const float disabled_torque[3] = {0.f, 0.f, 0.f};
+
+class Body_Manager
+{
+public:
+  Body_Manager(RobotController* robot_ctrl);
+  ~Body_Manager();
+
+  void init();
+  void run();
+  void setupStep();
+  void finalizeStep();
+  void initializeStateEstimator();
+
+  VectorNavData vectorNavData;
+  RobotControlParameters controlParameters;
+  ControlParameters* _userControlParameters = nullptr;
+  SpiData spiData;
+  SpiCommand spiCommand;
+  u64 _iterations = 0;
+
+  bool is_stand = false;
+
+private:
+  ros::NodeHandle _nh;
+
+  ros::Publisher _pub_low_cmd;
+  ros::Subscriber _sub_low_state;
+  void _lowStateCallback(unitree_legged_msgs::LowState msg);
+  void _torqueCalculator(SpiCommand* cmd, SpiData* data, spi_torque_t* torque_out, int board_num);
+
+  Quadruped<float> _quadruped;
+  FloatingBaseModel<float> _model;
+  LegController<float>* _legController = nullptr;
+  StateEstimatorContainer<float>* _stateEstimator;
+  StateEstimate<float> _stateEstimate;
+  RobotController* _robot_ctrl;
+  DesiredStateCommand<float>* _desiredStateCommand;
+  GamepadCommand driverCommand;
+  rc_control_settings rc_control;
+
+  spi_torque_t _spi_torque;
+
+  void _initSubscribers();
+  void _initPublishers();
+};
