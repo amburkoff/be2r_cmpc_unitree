3687b65115d72db376ac2985f205552a19791478
mr.Artem.Eg <aaegorov@itmo.ru>
2022-10-24 19:54:55 +0300

    rename files

---
+++ src/common/metrics/ContactEnergy.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Metric.h"
+
+using Eigen::Array4f;
+using Eigen::Array4i;
+
+// template <typename T>
+class ContactEnergy : public Metric 
+{
+public:
+  ContactEnergy();
+  ~ContactEnergy() = default;
+
+  Vec4<float> getFinalBodyCost();
+  Vec4<float> getFinalCost();
+  Vec4<float> getFinalLegCost();
+
+  // void setRobotData(ControlFSMData<T>& data);
+  // void debugPrint();
+//   void earlyContactHandle(Vec4<uint8_t> , int , int ) {}
+//   void restoreDefaults(){}
+  Vec3<float> g;
+
+private:
+  Vec4<float> _test;
+  Vec3<float> _vBody;
+  Vec3<float> _position;
+  Vec3<float> _state_coord;
+  LegControllerData<float> datas[4];
+  float _KinLinEnergy;
+  float _KinRotEnergy;
+  Vec4<float> _KinEnergyLeg;
+  float _PotEnergy;
+  Vec4<float> _PotEnergyLeg;
+};
